acdb4bada16797edc8820be8abd163e6b1d98c6b
mortargoblin <aaro.euro@gmail.com>
2026-03-18 11:26:22 +0200

    week1 js done

--- week_1/js_1/06/main.js
+++ week_1/js_1/06/main.js
@@ -1,16 +1,21 @@
-
 document.querySelector('#generateBtn')
   .addEventListener('click', (e) => {
     const num = 
-      Number(document.querySelector('#number'));
+      Number(document.querySelector('#number').value);
 
-    let result;
+    let result = '<table>';
 
     for (let i = 1; i <= num; i++) {
-      for (let j = 1; j >= num; j++) {
+      result += '<tr>';
+      for (let j = 1; j <= num; j++) {
+        console.log('iterated ' + i*j);
+        result += '<td>'+ i*j +'</td>';
       }
+      result += '</tr>';
     }
 
+    result += '</table>';
+
     document.querySelector('#output')
-      .textContent = result;
+      .innerHTML = result;
 });
