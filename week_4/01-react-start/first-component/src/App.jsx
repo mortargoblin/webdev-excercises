ada179c6b5ce7fe7788041642cc591428d7b0f45
mortargoblin <mortargoblin@github.com>
2026-04-07 17:18:38 +0300

    week_4

---
+++ week_4/01-react-start/first-component/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+      <h1>APP</h1>
+      <Home />
+    </>
+  );
+}
+
+export default App;
